6611f4c31506377132f7a6cf5a148ac3a0f9a724
Barrera <barreraalexander93@gmail.com>
2021-07-08 14:32:27 -0400

    mod menu is now working

--- website/static/js/interface_mods/menu.js
+++ website/static/js/interface_mods/menu.js
@@ -3,9 +3,31 @@ if (nav_button){
     nav_button.addEventListener('click', mod_menu, false)
 }
 
+
 function mod_menu(){
-    let hidden_menu = document.querySelector('#hidden_menu')
+    var hidden_menu = document.querySelector('#hidden_menu')
+    var menu_status = hidden_menu.dataset.status
 
-    hidden_menu.style.display = "block"
+    if (menu_status=='closed'){
+        hidden_menu.classList.remove('apply_hide_menu')
+        hidden_menu.classList.add('apply_show_menu')
+        hidden_menu.dataset.menu_status = 'open'
+        window.addEventListener('click', check_target, false)
+    } else if (menu_status=='open'){
+        window.removeEventListener('click', check_target)
+        menu_status = 'closed' 
+    }
+}
 
+
+function check_target(event){
+    if (event.target != hidden_menu &&
+        event.target != nav_button &&
+        event.target.parentNode != hidden_menu){
+        hidden_menu.dataset.status = 'closed'
+        hidden_menu.classList.remove('apply_show_menu')
+        hidden_menu.classList.add('apply_hide_menu')
+        menu_status = 'closed'
+        window.removeEventListener('click', check_target)
+    }
 }
