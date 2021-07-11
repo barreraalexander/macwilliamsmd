0956519fd4b5ac25bdecbd663de7127929d376cd
Barrera <barreraalexander93@gmail.com>
2021-07-11 12:25:39 -0400

    added map widget

--- website/components/service_panel.py
+++ website/components/service_panel.py
@@ -4,6 +4,7 @@ def component(service_name):
     img_sm_src = f"https://macwilliamsmd-static.s3.us-east-2.amazonaws.com/service_imgs/{service_name}_service_circle.png"
     img_lg_src = f"https://macwilliamsmd-static.s3.us-east-2.amazonaws.com/service_imgs/{service_name}_service_square.png"
     classes = ""
+    lazy_dataset = ""
 
     if service_name=="ekg":
         f_service_title = "EKG"
@@ -14,14 +15,19 @@ def component(service_name):
         classes = "inverse_service_panel"
         service_description = "This is a summary about the service. This is a summary about the service. This is a summary about the service. This is a summary about the service."
 
+
     elif service_name=="radiology":
         f_service_title = "Radiology"
         service_description = "This is a summary about the service. This is a summary about the service. This is a summary about the service. This is a summary about the service."
+        lazy_dataset = "loading='lazy'"
+
 
     elif service_name=="gynecology":
         f_service_title = "Gynecology"
         classes = "inverse_service_panel"
         service_description = "This is a summary about the service. This is a summary about the service. This is a summary about the service. This is a summary about the service."
+        lazy_dataset = "loading='lazy'"
+
 
     return Markup(f"""
         <div id="{service_name}_panel" class="service_ctnr {classes}">
@@ -29,8 +35,17 @@ def component(service_name):
                 <p class="sm_service_title">
                     {f_service_title}
                 </p>
-                <img class="sm_service_img" src="{img_sm_src}" alt="{service_name}" image>
-                <img class="lg_service_img" src="{img_lg_src}" alt="{service_name}" image>
+                <img
+                    class="sm_service_img"
+                    src="{img_sm_src}"
+                    alt="{service_name} image"
+                >
+                <img
+                    class="lg_service_img"
+                    src="{img_lg_src}"
+                    alt="{service_name} image"
+                    {lazy_dataset}
+                >
             </div>
             <div class="service_description">
                 <p class="lg_service_title">
